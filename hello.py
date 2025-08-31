b761c143e6dbbfb4eff07474a7874f2718a6c9a8
Yuvarani Muduru <yuvaranimuduru@gmail.com>
2025-08-31 07:53:20 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,111 @@
+print("hello")
+
+name =input("rani: ")
+print(name)
+age = input("17: ")
+
+            
+
+print(age)
+a = int(input("1: "))
+b = int(input("3: "))
+
+print("a = ", a)
+print ("b = ", b)
+print("sum =", a +b)
+print("spand")
+print("18:")
+#-------------
+print("# this is comment")
+"""
+this is line 1
+this is line 2
+this is line 3
+"""
+print("""
+this is line 1
+this is line 2
+this is line 3
+""")
+# single line comment: variable example
+name = "rajesh"
+age = 21
+height = 5.9
+
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+#single line comment: variable example
+is_student = True
+print("Is student:", is_student)
+
+# int
+age = 21
+print("Age:", age, type(age))
+
+# float
+height = 5.9
+print("Height:", height, type(height))
+
+# string
+name = "Rajesh"
+print("Name:", name, type(name))
+
+# bool
+is_student = True
+print("Is student:", is_student, type(is_student))
+
+# list
+fruits = ["apple", "banana", "cherry"]
+print("Fruits:", fruits, type(fruits))
+
+# tuple
+coordinates = (10,20)
+print("Coordinates:", coordinates, type(coordinates))
+
+# dictionary
+student = {"name": "Rajesh", "age":21, "is_student": True}
+print("Student dict:" , student, type(student))
+
+numbers = { 10,20,30,10,20}
+print("numbers:", numbers, type(numbers))
+
+# creating a set
+fruits = {"apple", "banana", "cherry", "apple"}
+print("Fruits set:", fruits, type(fruits))
+
+# add new item
+fruits.add("orange")
+print("After adding orange", fruits)
+
+# remove item
+fruits.remove("banana")
+print("after removing the banana:", fruits)
+
+x = 5
+if x < 10:
+     print("x is a small number")
+
+x = 15
+if x > 20:
+     print("x is greater than 20")
+else:
+     print("x is not greater than 20")
+
+x =15
+if x > 20:
+     print("x is greater than 20")
+elif x > 10:
+     print("x is greater than 10 but less than or equal to 20")
+else:
+     print("x is 10 or smaller")
+
+
+
+
+
+
+
+
+
